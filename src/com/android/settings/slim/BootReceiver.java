e8cd55251733564f5f1fcb44623853756cc9e313
Jorge Ruesga <jorge@ruesga.com>
2013-10-06 00:45:02 +0200

    Settings: Long Term Orbits (LTO)
    Change-Id: I1fbdba644fc05d55fc096b47bfac3f9903383370
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- src/com/android/settings/slim/BootReceiver.java
+++ src/com/android/settings/slim/BootReceiver.java
@@ -25,6 +25,7 @@ import com.android.settings.hardware.VibratorIntensity;
 import com.android.settings.inputmethod.InputMethodAndLanguageSettings;
 import com.android.settings.livedisplay.DisplayGamma;
 import com.android.settings.screwd.HardwareKeysSettings;
+import com.android.settings.location.LocationSettings;
 
 public class BootReceiver extends BroadcastReceiver {
     @Override
@@ -35,5 +36,6 @@ public class BootReceiver extends BroadcastReceiver {
         HardwareKeysSettings.restore(ctx);
         VibratorIntensity.restore(ctx);
         InputMethodAndLanguageSettings.restore(ctx);
+        LocationSettings.restore(ctx);
     }
 }
