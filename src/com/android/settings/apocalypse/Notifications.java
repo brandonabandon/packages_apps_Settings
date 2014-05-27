b52160ce2235051609faf62bda03f07838ff36b6
BigBrother1984 <stevewatersy@gmail.com>
2014-05-27 03:10:17 +0200

    [2/2] Add support for Peek app
    Disable built in peek when Peek app is installed. Enable it again on app's unistall.
    
    Signed-off-by: Carlo Savignano <stevewatersy@gmail.com>

--- src/com/android/settings/apocalypse/Notifications.java
+++ src/com/android/settings/apocalypse/Notifications.java
@@ -21,8 +21,13 @@ import android.app.ActivityManagerNative;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.admin.DevicePolicyManager;
+import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
@@ -72,6 +77,8 @@ import net.margaritov.preference.colorpicker.ColorPickerPreference;
 public class Notifications extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "NotificationSettings";
+	
+	 private static final String PEEK_APPLICATION = "com.jedga.peek";
     
     private static final String KEY_NOTIFICATION_PULSE = "notification_pulse";
     private static final String KEY_PEEK = "notification_peek";
@@ -115,6 +122,23 @@ public class Notifications extends SettingsPreferenceFragment implements
     private static final int DEFAULT_BACKGROUND_COLOR = 0x00ffffff;
     private static final int DEFAULT_TEXT_COLOR = 0xffffffff;
 	
+	private PackageStatusReceiver mPackageStatusReceiver;
+    private IntentFilter mIntentFilter;
+	
+	
+	private boolean isPeekAppInstalled() {
+        return isPackageInstalled(PEEK_APPLICATION);
+    }
+
+    private boolean isPackageInstalled(String packagename) {
+        PackageManager pm = getActivity().getPackageManager();
+        try {
+            pm.getPackageInfo(packagename, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+    }
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -285,18 +309,30 @@ public class Notifications extends SettingsPreferenceFragment implements
         Ringtone alert = RingtoneManager.getRingtone(getActivity(), ringtone);
         mReminderRingtone.setSummary(alert.getTitle(getActivity()));
         mReminderRingtone.setOnPreferenceChangeListener(this);
-        mReminderRingtone.setEnabled(mode != 0);		
+        mReminderRingtone.setEnabled(mode != 0);
+		
+		if (mPackageStatusReceiver == null) {
+            mPackageStatusReceiver = new PackageStatusReceiver();
+        }
+        if (mIntentFilter == null) {
+            mIntentFilter = new IntentFilter();
+            mIntentFilter.addAction(Intent.ACTION_PACKAGE_ADDED);
+            mIntentFilter.addAction(Intent.ACTION_PACKAGE_REMOVED);
+        }
+        getActivity().registerReceiver(mPackageStatusReceiver, mIntentFilter);		
     }
 	
 	
     @Override
     public void onResume() {
-        super.onResume();   
+        super.onResume();  
+		getActivity().registerReceiver(mPackageStatusReceiver, mIntentFilter); 
         updateState();
     }
     @Override
     public void onPause() {
         super.onPause();
+		getActivity().unregisterReceiver(mPackageStatusReceiver);
     }
     private void updateState() {
         
@@ -307,7 +343,8 @@ public class Notifications extends SettingsPreferenceFragment implements
 	private void updatePeekCheckbox() {
         boolean enabled = Settings.System.getInt(getContentResolver(),
                 Settings.System.PEEK_STATE, 0) == 1;
-        mNotificationPeek.setChecked(enabled);
+        mNotificationPeek.setChecked(enabled && !isPeekAppInstalled());
+        mNotificationPeek.setEnabled(!isPeekAppInstalled());
     }
     
     @Override
@@ -544,4 +581,18 @@ public class Notifications extends SettingsPreferenceFragment implements
         mReminderInterval.setValue(Integer.toString(value));
         mReminderInterval.setSummary(getResources().getString(resId));
     }
+	
+	public class PackageStatusReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(Intent.ACTION_PACKAGE_ADDED)) {
+                updatePeekCheckbox();
+            } else if(action.equals(Intent.ACTION_PACKAGE_REMOVED)) {
+                updatePeekCheckbox();
+            }
+        }
+    }
+	
+
 }
