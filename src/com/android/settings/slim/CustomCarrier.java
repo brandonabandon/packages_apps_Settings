f83be559a664ac0128589bab9f772f96c1dcbeb4
MrApocalypse <ensabahnur16@gmail.com>
2014-08-09 09:44:57 -0500

    Move CustomCarrier.java to the slim directory

--- src/com/android/settings/slim/CustomCarrier.java
+++ src/com/android/settings/slim/CustomCarrier.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settings.AOSPAL;
+package com.android.settings.slim;
 
 import android.content.Context;
 import android.preference.EditTextPreference;
