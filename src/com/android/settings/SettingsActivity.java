833da4fe50844a3182a7afcc4ec0ced716f6d7fc
Michael Bestas <mikeioannina@gmail.com>
2014-12-10 23:58:23 +0200

    Settings: Fix blacklist settings crash
    Change-Id: Idd261aa9f8e2867312aaaece26150671b177994c

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -300,7 +300,7 @@ public class SettingsActivity extends Activity
             OtherSoundSettings.class.getName(),
             QuickLaunchSettings.class.getName(),
             ApnSettings.class.getName(),
-			QSTiles.class.getName()
+			QSTiles.class.getName(),
             BlacklistSettings.class.getName()
     };
 
