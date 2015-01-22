4c61b1fe01baa6ea3d7590e3767dd3413d7d4f5b
Adnan Begovic <adnan@cyngn.com>
2015-01-22 12:23:59 -0800

    Settings: Add LteTile.
    Change-Id: I696624dd2637cf89cc574b727e1e04f24270ab27

--- src/com/android/settings/screwd/qs/QSTileHolder.java
+++ src/com/android/settings/screwd/qs/QSTileHolder.java
@@ -17,6 +17,7 @@ package com.android.settings.screwd.qs;
 
 import android.content.Context;
 
+import android.text.TextUtils;
 import com.android.internal.util.cm.QSConstants;
 import com.android.internal.util.cm.QSUtils;
 import com.android.settings.R;
@@ -44,6 +45,18 @@ public class QSTileHolder {
 
         if (!TILE_ADD_DELETE.equals(tileType) &&
                 !QSUtils.getAvailableTiles(context).contains(tileType)) {
+            return null;
+        }
+
+        // We need to filter out the LTE tile manually, because
+        // filtering via getAvailableTiles during fwb init
+        // disallows reading our system prop
+        // Hide the tile if device doesn't support LTE
+        // or it supports Dual Sim Dual Active.
+        // TODO: Should be spawning off a tile per sim
+        if (TextUtils.equals(QSConstants.TILE_LTE, tileType)
+                && (!QSUtils.deviceSupportsLte(context)
+                || QSUtils.deviceSupportsDdsSupported(context))) {
             return null;
         }
 
@@ -93,6 +106,10 @@ public class QSTileHolder {
             case QSConstants.TILE_NOTIFICATIONS:
                 resourceName = "ic_qs_ringer_audible";
                 stringId = R.string.qs_tile_notifications;
+                break;
+            case QSConstants.TILE_LTE:
+                resourceName = "ic_qs_lte_on";
+                stringId = R.string.qs_tile_lte;
                 break;
             case QSConstants.TILE_NFC:
                 resourceName = "ic_qs_nfc_on";
