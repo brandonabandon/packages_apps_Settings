dc52d10f7a7f24969d47e4a9a70a7f6ea88a7ac3
MrApocalypse <ensabahnur16@gmail.com>
2015-01-21 23:15:53 -0600

    Whoops!!

--- src/com/android/settings/applications/ExpandedDesktopPreferenceFragment.java
+++ src/com/android/settings/applications/ExpandedDesktopPreferenceFragment.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.settings.screwd.fragments;
+package com.android.settings.applications;
 
 import android.annotation.Nullable;
 import android.content.ContentResolver;
