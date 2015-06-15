63d6999e11d1db2f0a1a3267c9ac8a45aa2495c5
Corey Edwards <ensabahnur16@gmail.com>
2015-06-15 18:02:44 -0500

    Remove Navbar Tile(2/2)
    ...also, remove the Enable/Disable switch. I give up.

--- src/com/android/settings/screwd/qs/QSTileHolder.java
+++ src/com/android/settings/screwd/qs/QSTileHolder.java
@@ -162,10 +162,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_powermenu";
                 stringId = R.string.qs_tile_powermenu;
                 break;
-			case QSConstants.TILE_NAVBAR:
+			/*case QSConstants.TILE_NAVBAR:
                 resourceName = "ic_qs_navbar_on";
                 stringId = R.string.qs_navbar_tile;
-                break;
+                break;*/
             case QSConstants.TILE_APPCIRCLEBAR:
                 resourceName = "ic_qs_appcirclebar_on";
                 stringId = R.string.qs_appcirclebar_tile;
