9a14fcc39a62e3860933089b0ae738b349b68367
Daniel Koman <dankoman30@gmail.com>
2015-07-25 11:40:55 -0600

    Settings: fix dslv icon size
    Change-Id: Ic226192dd2eed6a720da0db02ff9e0dd33ae3519

--- src/com/android/settings/screwd/dslv/ActionListViewSettings.java
+++ src/com/android/settings/screwd/dslv/ActionListViewSettings.java
@@ -664,7 +664,7 @@ public class ActionListViewSettings extends ListFragment implements
                     && iconUri.startsWith(ActionConstants.SYSTEM_ICON_IDENTIFIER))) {
                 if (d != null) d.setTint(getResources().getColor(R.color.dslv_icon_dark));
             }
-            holder.iconView.setImageDrawable(d);
+            holder.iconView.setImageBitmap(ImageHelper.drawableToBitmap(d));
 
             if (!mDisableIconPicker && holder.iconView.getDrawable() != null) {
                 holder.iconView.setOnClickListener(new OnClickListener() {
